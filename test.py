a1424965b3b9aa5beee35582f19c99a4ffa5b91c
hohovisual <hellomr0111@gmail.com>
2023-02-25 17:44:23 +0900

    20230224 second modeling

--- test.py
+++ test.py
@@ -7,18 +7,34 @@ import pandas as pd
 import glob
 import re
 import sentencepiece as spm
+from torch.utils.data import Dataset
 import numpy as np
 import os
 from sklearn.model_selection import train_test_split
 from tqdm.auto import tqdm
-from Utils.preprocess import preprocess
+from Utils.preprocess import *
+from Data.dataset import *
+from Model.model_transformer import *
 
-def test_vocab(file_path):
+d_model = 512
+heads = 8
+num_layers = 6
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+epochs = 10
+
+train_loader = torch.utils.data.DataLoader(Dataset(),
+                                           batch_size = 100, 
+                                           shuffle=True, 
+                                           pin_memory=True)
+
+def load_split(file_path):
     data = pd.read_csv(file_path, encoding = 'utf8')
     print(len(data))
 
     Q, A = preprocess(data)
+    return Q, A
 
+def test_vocab():
     corpus = "total.txt"
     prefix = "chatbot"
     vocab_size = 16000
@@ -43,9 +59,68 @@ def test_vocab(file_path):
 
     return line, pieces, ids
 
+def train(train_loader, transformer, criterion, epoch):
+    
+    transformer.train()
+    sum_loss = 0
+    count = 0
+
+    for i, (question, reply) in enumerate(train_loader):
+        
+        samples = question.shape[0]
+
+        # Move to device
+        question = question.to(device)
+        reply = reply.to(device)
+
+        # Prepare Target Data
+        reply_input = reply[:, :-1]
+        reply_target = reply[:, 1:]
+
+        # Create mask and add dimensions
+        question_mask, reply_input_mask, reply_target_mask = create_masks(question, reply_input, reply_target)
+
+        # Get the transformer outputs
+        out = transformer(question, question_mask, reply_input, reply_input_mask)
+
+        # Compute the loss
+        loss = criterion(out, reply_target, reply_target_mask)
+        
+        # Backprop
+        transformer_optimizer.optimizer.zero_grad()
+        loss.backward()
+        transformer_optimizer.step()
+        
+        sum_loss += loss.item() * samples
+        count += samples
+        
+        if i % 100 == 0:
+            print("Epoch [{}][{}/{}]\tLoss: {:.3f}".format(epoch, i, len(train_loader), sum_loss/count))
+
+
 
 if __name__ == '__main__':
-    line, pieces, ids = test_sentencepiece('Data/gamsung.csv')
-    print(line)
-    print(pieces)
-    print(ids)
+    d_model = 512
+    heads = 8
+    num_layers = 1
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    epochs = 10
+    Q, A = load_split('Data/gamsung.csv')
+    line, pieces, ids = test_vocab()
+    lr = 1e-4
+    with open('Data/gamsung.json', 'r') as j:
+        word_map = json.load(j)
+    model = Transformer(d_model = d_model, heads = heads, num_layers = num_layers, word_map = word_map)
+    model = model.to(device)
+    adam_optimizer = torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9)
+    transformer_optimizer = AdamWarmup(model_size = d_model, warmup_steps = 4000, optimizer = adam_optimizer)
+    criterion = LossWithLS(len(word_map), 0.1)
+    criterion_reduction_none = nn.MSELoss(reduction='none')
+    
+
+    for epoch in range(epochs):
+        
+        train(train_loader, model, criterion_reduction_none, epoch)
+        
+        state = {'epoch': epoch, 'transformer': model, 'transformer_optimizer': transformer_optimizer}
+        torch.save(state, 'checkpoint_' + str(epoch) + '.pth.tar')
